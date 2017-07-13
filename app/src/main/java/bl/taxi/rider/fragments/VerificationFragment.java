2eb3e41dbf8b8ac1cd401040d5bb52030e5db4af
STR <str@cogzidel.com>
2017-07-13 18:58:52 +0530

    Changes in Verification fragment -- STR

---
+++ app/src/main/java/bl/taxi/rider/fragments/VerificationFragment.java
@@ -0,0 +1,30 @@
+package bl.taxi.rider.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import bl.taxi.rider.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class VerificationFragment extends Fragment {
+
+
+    public VerificationFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_verification, container, false);
+    }
+
+}
