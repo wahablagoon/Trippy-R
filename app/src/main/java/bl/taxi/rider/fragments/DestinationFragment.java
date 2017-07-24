325ec3894e3a6e4b3ac70e3fd384d570817ec349
Vignesh <vignesh@bluelagoontechnologies.com>
2017-07-24 19:54:00 +0530

    added fragment destination

---
+++ app/src/main/java/bl/taxi/rider/fragments/DestinationFragment.java
@@ -0,0 +1,45 @@
+package bl.taxi.rider.fragments;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import bl.taxi.rider.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link DestinationFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class DestinationFragment extends Fragment {
+
+    public DestinationFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment.
+     *
+     * @return A new instance of fragment DestinationFragment.
+     */
+    public static DestinationFragment newInstance() {
+        return new DestinationFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_destination, container, false);
+    }
+
+}
