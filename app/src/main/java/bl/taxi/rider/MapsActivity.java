9d5b1a2126af6f97c86e95a3d8028286f4dbdcb2
Vignesh <vignesh@bluelagoontechnologies.com>
2017-07-25 20:39:01 +0530

    added drawer.

--- app/src/main/java/bl/taxi/rider/MapsActivity.java
+++ app/src/main/java/bl/taxi/rider/MapsActivity.java
@@ -3,11 +3,13 @@ package bl.taxi.rider;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
-import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -20,6 +22,14 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
 import bl.taxi.rider.utils.InternetUtils;
 import bl.taxi.rider.utils.PermissionUtils;
@@ -28,21 +38,55 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener,
         GoogleMap.OnMyLocationButtonClickListener {
 
-    private GoogleMap mMap;
-    private GoogleApiClient googleApiClient;
-    Location mCurrentLocation;
-    int LOCATION_PERMISSION_REQUEST_CODE = 1;
     private static final int GOOGLE_DEFAULT_ZOOM = 15;
-
     /**
      * Flag indicating whether a permission is already requested or not
      */
     private static boolean mPermissionRequested = false;
+    Location mCurrentLocation;
+    int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    ImageView drawer_menu;
+    Drawer drawer;
+    private GoogleMap mMap;
+    private GoogleApiClient googleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        // Create the AccountHeader
+        AccountHeader headerResult = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withHeaderBackground(R.mipmap.splash_booking)
+                .addProfiles(
+                        new ProfileDrawerItem().withName("Mike Penz").withEmail("mikepenz@gmail.com").withIcon(getResources().getDrawable(R.drawable.ic_account_circle))
+                )
+                /*.withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
+                    @Override
+                    public boolean onProfileChanged(View view, IProfile profile, boolean currentProfile) {
+                        return false;
+                    }
+                })*/
+                .build();
+
+        drawer = new DrawerBuilder()
+                .withActivity(this)
+                .withAccountHeader(headerResult)
+                .withSelectedItem(-1)
+                /*.addDrawerItems(
+                        new PrimaryDrawerItem().withIdentifier(1).withName(R.string.drawer_item_home);,
+                        new DividerDrawerItem(),
+                        item2,
+                        new SecondaryDrawerItem().withName(R.string.drawer_item_settings)
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        // do something with the clicked item :D
+                    }
+                })*/
+                .build();
 
         //Connect Googleclient Location API
         googleApiClient = new GoogleApiClient.Builder(MapsActivity.this)
@@ -55,14 +99,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
+    public void onConnected(@Nullable Bundle bundle) {
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
 
-        //Disconnect Googleclient Location API and remove Listeners for this Activity
-        LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient, this);
+    @Override
+    public void onConnectionSuspended(int i) {
 
-        if (googleApiClient != null && googleApiClient.isConnected())
-            googleApiClient.disconnect();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(MapsActivity.this, R.string.google_api_connection_fail, Toast.LENGTH_LONG).show();
     }
 
     /**
@@ -84,6 +135,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
 
         mMap.setOnMyLocationButtonClickListener(this);
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false;
     }
 
     /**
@@ -133,6 +191,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     @Override
+    public void onLocationChanged(Location location) {
+        mCurrentLocation = location;
+    }
+
+    @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
                                            @NonNull int[] grantResults) {
         if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
@@ -147,6 +210,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             // Display the missing permission error dialog when the fragments resume.
             showMissingPermissionError();
         }
+    }
+
+    /**
+     * Displays a dialog with error message explaining that the location permission is missing.
+     */
+    private void showMissingPermissionError() {
+        PermissionUtils.PermissionDeniedDialog
+                .newInstance(getString(R.string.location_permission_denied), true).show(getSupportFragmentManager(), "dialog");
     }
 
     @Override
@@ -173,41 +244,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
-    /**
-     * Displays a dialog with error message explaining that the location permission is missing.
-     */
-    private void showMissingPermissionError() {
-        PermissionUtils.PermissionDeniedDialog
-                .newInstance(getString(R.string.location_permission_denied), true).show(getSupportFragmentManager(), "dialog");
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        //Disconnect Googleclient Location API and remove Listeners for this Activity
+        LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient, this);
+
+        if (googleApiClient != null && googleApiClient.isConnected())
+            googleApiClient.disconnect();
     }
 
     @Override
-    public boolean onMyLocationButtonClick() {
-        // Return false so that we don't consume the event and the default behavior still occurs
-        // (the camera animates to the user's current position).
-        return false;
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen()) {
+            drawer.closeDrawer();
+        } else {
+            super.onBackPressed();
+        }
     }
 
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        Toast.makeText(MapsActivity.this, R.string.google_api_connection_fail, Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-
+    public void drawerMenu(View view) {
+        if (!drawer.isDrawerOpen())
+            drawer.openDrawer();
     }
 }
